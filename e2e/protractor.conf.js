3444c720d117d8cacd1550954cc4d2e8abeeae19
Kyle Nichting <nichtingkyle@gmail.com>
2020-05-18 13:26:12 -0400

    leaving out e2e for eslint

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,7 +2,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-import { SpecReporter } from 'jasmine-spec-reporter';
+const { SpecReporter } = require('jasmine-spec-reporter');
 
 /**
  * @type { import("protractor").Config }
